7d4c35f916db4f9dcd18c6d7e8dff432ba1fae6f
Santiago <SPWebDesigns@users.noreply.github.com>
2024-04-11 11:03:17 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+const cowsay = require('cowsay');
+function helloWorldArmadilloCCS() {
+  console.log(cowsay.say({ text: "hello world", f: "armadilloCCS" }));
+}
+module.exports = helloWorldArmadillo;
